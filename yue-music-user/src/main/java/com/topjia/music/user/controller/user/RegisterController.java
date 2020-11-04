f6212e32c270391c7eccf32ef36ab548bfa37903
topjia <topjia@qq.vip.com>
2020-11-04 14:44:16 +0800

    修改用户注册api接口

--- yue-music-user/src/main/java/com/topjia/music/user/controller/user/RegisterController.java
+++ yue-music-user/src/main/java/com/topjia/music/user/controller/user/RegisterController.java
@@ -5,8 +5,10 @@ import com.topjia.music.user.domain.dto.user.UserRegisterDTO;
 import com.topjia.music.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author topjia
